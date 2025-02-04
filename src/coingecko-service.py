0a529f89c70a3398086ce1d5dd435658cdea0f3e
PJ <pazuzujindrich@gmail.com>
2025-02-04 11:45:57 -0800

    added coingecko-service to get coin data by id

---
+++ src/coingecko-service.py
@@ -0,0 +1,34 @@
+import os
+
+import requests
+
+domain = os.getenv('COINGECKO_DOMAIN')
+api_key = os.getenv('COINGECKO_KEY')
+
+if not domain:
+    raise ValueError("Missing environment variable: COINGECKO_DOMAIN")
+if not api_key:
+    raise ValueError("Missing environment variable: COINGECKO_KEY")
+
+headers = {
+    "accept": "application/json",
+    "x_cg_demo_api_key": api_key
+}
+
+
+def get_coin_data_by_id(coin_id):
+    url = f"{domain}/coins/{coin_id}"
+    params = {
+        "localization": False,
+        "tickers": False,
+        "community_data": False,
+        "developer_data": False,
+        "sparkline": False
+    }
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        return None
